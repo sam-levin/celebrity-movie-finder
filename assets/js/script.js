68d312be9f0b299a73b7a15056b020dab7aeb9e6
stevey d <stphndcky22@gmail.com>
2022-03-16 14:16:38 -0700

    created dynamic search based on user input, added event listener to run function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,45 +3,93 @@
 
 // variable declarations
 
-var key = "k_01ly574i";
+var key = "k_01ly574i"
 
-var ninjasUrl = "https://api.api-ninjas.com/v1/celebrity?name=LeonardoDiCaprio";
+var ninjasUrl = "https://api.api-ninjas.com/v1/celebrity?name="
 
-var ninjasKey = "EbyKJN6Fx+lZBTlMbCLTSw==0t4uz4G3aPpnWFgY";
+var ninjasKey = "EbyKJN6Fx+lZBTlMbCLTSw==0t4uz4G3aPpnWFgY"
 
-var url = "https://imdb-api.com/en/API/SearchName/k_01ly574i/LeonardoDiCaprio";
+var submitBtn = document.getElementById("submit-btn");
 
-// imdb API fetch
+var inputEl = document.querySelector("#actor-input");
 
-fetch(url).then(function(response) {
-    response.json().then(function(data) {
-        console.log(data);
-        console.log(data.results[0].id);
-        var actorId = data.results[0].id
-        var secondUrl = "https://imdb-api.com/API/Name/k_01ly574i/LeonardoDiCaprio";
-        fetch(secondUrl).then(function(response) {
-            response.json().then(function(data) {
-                console.log(data);
-                console.log(data.image);
-                console.log(data.knownFor);
-            })
-        });
-    })
-})
+var mainContainer = document.querySelector(".main-container");
+
+var actorPicContainer = document.querySelector(".pic-container");
+
+// buttonHandler function
 
 
-// celebrity API fetch 
+var buttonHandler = function (event) {
 
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/celebrity?name=LeonardoCiCaprio',
-    headers: { 'X-Api-Key': 'EbyKJN6Fx+lZBTlMbCLTSw==0t4uz4G3aPpnWFgY'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
 
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-}); 
+    // prevent page refresh
+
+    event.preventDefault();
+    
+    var actorName = inputEl.value.trim();
+
+    // dynamic search based on user input
+
+    var url = "https://imdb-api.com/en/API/SearchName/k_01ly574i/" + actorName;
+
+    fetch(url).then(function(response) {
+
+        response.json().then(function(data) {
+
+            console.log(data);
+
+            console.log(data.results[0].id);
+
+            var actorId = data.results[0].id
+
+            // now searching imdb with newly acquired actor id
+
+            var secondUrl = "https://imdb-api.com/API/Name/k_01ly574i/" + actorId;
+
+            fetch(secondUrl).then(function(response) {
+
+                response.json().then(function(data) {
+
+                    // we obtain info about the movies they're known for, and an image
+
+                    console.log(data);
+
+                    console.log(data.image);
+
+                    console.log(data.knownFor);
+                
+                })
+
+            });
+        })
+    });
+
+    // fetching celebrity API data with user input
+
+    $.ajax({
+
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/celebrity?name=' + actorName,
+        headers: { 'X-Api-Key': 'EbyKJN6Fx+lZBTlMbCLTSw==0t4uz4G3aPpnWFgY'},
+        contentType: 'application/json',
+
+        // succesful request will console log data we want
+
+        success: function(result) {
+
+            console.log(result);
+
+
+        },
+        error: function ajaxError(jqXHR) {
+
+            console.error('Error: ', jqXHR.responseText);
+        }
+    }); 
+    
+}
+
+// an event listener for our submit button
+
+submitBtn.addEventListener("click", buttonHandler);
