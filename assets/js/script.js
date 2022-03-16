6e05ebd644acbb5254bb0bc45b9d0f15a215a354
stevey d <stphndcky22@gmail.com>
2022-03-16 14:10:15 -0700

    fetch request to both APIs return data that is console logged

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,47 @@
 // script file to see fetch for imdb, celebrity api
 
-var IMDBurl = "https://imdb-api.com/en/API/FullCast/k_c2a3w0b6/tt1375666"
 
-var key = "k_c2a3w0b6"
+// variable declarations
 
-var ninjasUrl = "https://api.api-ninjas.com/v1/celebrity?name=LeonardoDiCaprio"
+var key = "k_01ly574i";
 
-var ninjasKey = "EbyKJN6Fx+lZBTlMbCLTSw==0t4uz4G3aPpnWFgY"
+var ninjasUrl = "https://api.api-ninjas.com/v1/celebrity?name=LeonardoDiCaprio";
 
-var mainEl = $("#main-content")
+var ninjasKey = "EbyKJN6Fx+lZBTlMbCLTSw==0t4uz4G3aPpnWFgY";
 
-var actorName = $("#actor-name").val();
+var url = "https://imdb-api.com/en/API/SearchName/k_01ly574i/LeonardoDiCaprio";
 
-var testActorName = 'Leonardo DiCaprio'
+// imdb API fetch
 
-var printActorDetails = function(actorName, actorData){
-    var actorNameEl = $("<h5>").text(actorName)
-    var actorHeightEl = $("<h5>").text("Height:" + actorData[0].height + " meters ")
-    var actorAgeEl = $("<h5>").text("Age: " + actorData[0].age + " years old")
-    mainEl.append(actorNameEl, actorHeightEl, actorAgeEl)
-}
-
-fetch(IMDBurl).then(function(response) {
+fetch(url).then(function(response) {
     response.json().then(function(data) {
         console.log(data);
+        console.log(data.results[0].id);
+        var actorId = data.results[0].id
+        var secondUrl = "https://imdb-api.com/API/Name/k_01ly574i/LeonardoDiCaprio";
+        fetch(secondUrl).then(function(response) {
+            response.json().then(function(data) {
+                console.log(data);
+                console.log(data.image);
+                console.log(data.knownFor);
+            })
+        });
     })
-});
+})
+
+
+// celebrity API fetch 
 
 $.ajax({
     method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/celebrity?name=' + testActorName,
+    url: 'https://api.api-ninjas.com/v1/celebrity?name=LeonardoCiCaprio',
     headers: { 'X-Api-Key': 'EbyKJN6Fx+lZBTlMbCLTSw==0t4uz4G3aPpnWFgY'},
     contentType: 'application/json',
     success: function(result) {
         console.log(result);
-        printActorDetails(testActorName, result)
+
     },
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
     }
-});
-
-
-// This code isnt working atm
-/*$("#actor-form-input").on("click", "button",function(event){
-    event.preventDefault();
-    var inputActor = $("#actor-name").val()
-    $("#actor-name").val("")
-    if (inputActor == "") {
-        window.alert("Please type in a city name or choose from one of your past searches")
-    } else {
-        console.log(inputActor)
-    }
-}) */
+}); 
