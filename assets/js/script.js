1c802a5d4a26b20aeb48e4588c3034aa67ca7d3d
stevey d <stphndcky22@gmail.com>
2022-03-16 20:42:48 -0700

    added error handling/modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,11 +83,37 @@ var buttonHandler = function (event) {
 
             else {
 
-                mainDiv.classList.remove("has-background-dark");
-                
-                window.alert("Whoops! Something went wrong. Please try again.")
+                // When the user clicks on the button, open the modal
 
+                mainDiv.classList.remove("has-background-dark");
+
+                // Get the modal
+
+                var modal = document.getElementById("myModal");
+
+                // Get the <span> element that closes the modal
+
+                var span = document.getElementsByClassName("close")[0];
+
+                modal.style.display = "flex";
+
+                // When the user clicks on <span> (x), close the modal
+
+                span.onclick = function() {
+
+                    modal.style.display = "none";
+                }
+
+                // When the user clicks anywhere outside of the modal, close it
                 
+                window.onclick = function(event) {
+
+                    if (event.target == modal) {
+
+                    modal.style.display = "none";
+
+                    }
+                }
 
             }
 
@@ -209,6 +235,44 @@ var buttonHandler = function (event) {
             // obtain facts about actor from Celebrity API
 
             console.log(result);
+
+            if (result.length === 0) {
+
+                // When the user clicks on the button, open the modal
+
+                mainDiv.classList.remove("has-background-dark");
+
+                // Get the modal
+  
+                var modal = document.getElementById("myModal");
+  
+                // Get the <span> element that closes the modal
+  
+                var span = document.getElementsByClassName("close")[0];
+  
+                modal.style.display = "flex";
+  
+                // When the user clicks on <span> (x), close the modal
+  
+                span.onclick = function() {
+  
+                    modal.style.display = "none";
+
+                }
+  
+                // When the user clicks anywhere outside of the modal, close it
+                  
+                window.onclick = function(event) {
+  
+                    if (event.target == modal) {
+  
+                        modal.style.display = "none";
+  
+                    }
+
+                }
+
+            }
 
             var actorInfoContainer = document.createElement("div");
 
